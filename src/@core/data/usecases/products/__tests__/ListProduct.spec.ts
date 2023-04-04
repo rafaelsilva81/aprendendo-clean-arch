c2c4642fd827352c411307e0c32a0c346bd8a447
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-04-04 10:51:21 -0300

    chore: reorganize files

--- src/@core/data/usecases/products/__tests__/ListProduct.spec.ts
+++ src/@core/data/usecases/products/__tests__/ListProduct.spec.ts
@@ -1,17 +1,8 @@
-import { HttpGetClient } from "../../protocols/http/http-get-client";
-import { ListProduct } from "./ListProducts";
+import { HttpClientSpy } from "../../../protocols/http/__tests__/http-client.mock";
+import { ListProduct } from "../ListProducts";
 
 describe("ListProduct", () => {
   test("should call httpclient with correct url", async () => {
-    class HttpClientSpy implements HttpGetClient {
-      url?: string;
-
-      async get(url: string) {
-        this.url = url;
-        return Promise.resolve();
-      }
-    }
-
     const url = "any_url";
     const httpClient = new HttpClientSpy();
     const sut = new ListProduct(url, httpClient);
