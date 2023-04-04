fd8e465b6be9fbabb874261b3ae8f6062a2ebc03
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-04-04 10:59:53 -0300

    refactor: move SUT creation to a factory method

--- src/@core/data/usecases/products/__tests__/ListProduct.spec.ts
+++ src/@core/data/usecases/products/__tests__/ListProduct.spec.ts
@@ -1,14 +1,26 @@
 import { HttpClientSpy } from "../../../../mocks/http-client.mock";
 import { ListProduct } from "../ListProducts";
 
+type SutTypes = {
+  sut: ListProduct;
+  httpClient: HttpClientSpy;
+};
+
+const makeSut = (url: string = "any_url"): SutTypes => {
+  const httpClient = new HttpClientSpy();
+  const sut = new ListProduct(url, httpClient);
+
+  return {
+    sut,
+    httpClient,
+  };
+};
+
 describe("ListProduct", () => {
   test("should call httpclient with correct url", async () => {
-    const url = "any_url";
-    const httpClient = new HttpClientSpy();
-    const sut = new ListProduct(url, httpClient);
-
+    const url = "other_url";
+    const { sut, httpClient } = makeSut(url);
     await sut.execute();
-
     expect(httpClient.url).toBe(url);
   });
 });
